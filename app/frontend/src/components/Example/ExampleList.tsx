07975e44c4de8dceaabef2410466e45e9a47cde4
crazedout <73832303+crazedout@users.noreply.github.com>
2023-05-29 12:55:55 +0000

    Replaced one file, renamed one and added new pdfs.

--- app/frontend/src/components/Example/ExampleList.tsx
+++ app/frontend/src/components/Example/ExampleList.tsx
@@ -9,11 +9,23 @@ export type ExampleModel = {
 
 const EXAMPLES: ExampleModel[] = [
     {
-        text: "What is included in my Northwind Health Plus plan that is not in standard?",
-        value: "What is included in my Northwind Health Plus plan that is not in standard?"
+        //text: "What is included in my Northwind Health Plus plan that is not in standard?",
+        //value: "What is included in my Northwind Health Plus plan that is not in standard?"
+        text: "Hur återställer jag mitt lösenord för Canvas?",
+        value: "Hur återställer jag mitt lösenord för Canvas?"
     },
-    { text: "What happens in a performance review?", value: "What happens in a performance review?" },
-    { text: "What does a Product Manager do?", value: "What does a Product Manager do?" }
+    { 
+        //text: "What happens in a performance review?", 
+        //value: "What happens in a performance review?" 
+        text: "Hur ansluter jag mig till eduroam?", 
+        value: "Hur ansluter jag mig till eduroam?" 
+    },
+    { 
+        //text: "What does a Product Manager do?", 
+        //value: "What does a Product Manager do?"
+        text: "Skriv en informativ Rap Battle om Högskolan Väst", 
+        value: "Skriv en informativ Rap Battle om Högskolan Väst" 
+    }
 ];
 
 interface Props {
